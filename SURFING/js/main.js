3912c11f540c0666ac1346c6f17f65f6ae5156c5
timur <gulamjanov4@gmail.com>
2019-01-30 17:22:01 +0600

    delete

--- SURFING/js/main.js
+++
@@ -1,5 +0,0 @@
-$('.menu-phone').on('click', function(){
-   $('.nav-wrapper').toggleClass('menu-phone__active');
-   $('.nav').toggleClass('display-flex');
-   $('.logo').toggleClass('display-none');
-})
