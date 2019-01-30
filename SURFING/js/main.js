4cddfcc597a425b6e55ae5652813d0db289f38f1
timur <gulamjanov4@gmail.com>
2019-01-30 17:17:16 +0600

    update

---
+++ SURFING/js/main.js
@@ -0,0 +1,5 @@
+$('.menu-phone').on('click', function(){
+   $('.nav-wrapper').toggleClass('menu-phone__active');
+   $('.nav').toggleClass('display-flex');
+   $('.logo').toggleClass('display-none');
+})
